eb18e62509a9908de6836bc7d3520536b104f537
Ayinla Abel <kraneabel@gmail.com>
2022-09-05 03:13:15 -0700

    console.py updated

--- console.py
+++ console.py
@@ -62,13 +62,13 @@ class HBNBCommand(cmd.Cmd):
             "update": self.do_update
         }
         match = re.search(r"\.", arg)
-        if match is not None:
+       if match is not None:
             argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
             match = re.search(r"\((.*?)\)", argl[1])
             if match is not None:
                 command = [argl[1][:match.span()[0]], match.group()[1:-1]]
                 if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
+                   call = "{} {}".format(argl[0], command[1])
                     return argdict[command[0]](call)
         print("*** Unknown syntax: {}".format(arg))
         return False
@@ -112,7 +112,6 @@ class HBNBCommand(cmd.Cmd):
         else:
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
-
     def do_destroy(self, arg):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
@@ -135,8 +134,7 @@ class HBNBCommand(cmd.Cmd):
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects."""
         argl = parse(arg)
-
-       if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
             objl = []
@@ -144,7 +142,7 @@ class HBNBCommand(cmd.Cmd):
                 if len(argl) > 0 and argl[0] == obj.__class__.__name__:
                     objl.append(obj.__str__())
                 elif len(argl) == 0:
-                    objl.append(obj.__str__())
+                   objl.append(obj.__str__())
             print(objl)
 
     def do_count(self, arg):
@@ -154,7 +152,7 @@ class HBNBCommand(cmd.Cmd):
         count = 0
         for obj in storage.all().values():
             if argl[0] == obj.__class__.__name__:
-                count += 1
+               count += 1
         print(count)
 
     def do_update(self, arg):
@@ -164,7 +162,7 @@ class HBNBCommand(cmd.Cmd):
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary."""
         argl = parse(arg)
-        objdict = storage.all()
+       objdict = storage.all()
 
         if len(argl) == 0:
             print("** class name missing **")
